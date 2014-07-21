4cbb5acef6b51b2c2f12d8de9480aa4ecf034bce
Guillaume Denry <guillaume.denry@gmail.com>
2014-07-21 10:56:58 +0200

    yeoman generated files

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name debriefilatorApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the debriefilatorApp
+ */
+angular.module('debriefilatorApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name debriefilatorApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the debriefilatorApp
+ */
+angular.module('debriefilatorApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
