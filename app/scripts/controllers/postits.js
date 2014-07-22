24ff7bcd48af8d1cd37ac9e997ae61ad021d1996
Guillaume Denry <guillaume.denry@gmail.com>
2014-07-22 10:06:34 +0200

    contenteditable div are now binded with their models

--- app/scripts/controllers/postits.js
+++ app/scripts/controllers/postits.js
@@ -19,3 +19,48 @@ app.controller('PostitsCtrl', function ($scope, Postit) {
 		Postit.delete(postitType, index);
 	};
 });
+
+// the following directive is used to bind contenteditable div with an angular model
+app.directive('contenteditable', function() {
+	return {
+		restrict: 'A', // only activate on element attribute
+		require: '?ngModel', // get a hold of NgModelController
+		link: function(scope, element, attrs, ngModel) {
+			if (!ngModel) return; // do nothing if no ng-model
+
+			// Specify how UI should be updated
+			ngModel.$render = function() {
+				element.html(ngModel.$viewValue || '');
+			};
+
+			// Listen for change events to enable binding
+			element.on('blur keyup change', function() {
+				scope.$apply(read);
+			});
+
+			// Write data to the model
+			function read() {
+				var html = element.html();
+				// When we clear the content editable the browser leaves a <br> behind
+				// If strip-br attribute is provided then we strip this out
+				if (attrs.stripBr && html == '<br>') {
+					html = '';
+				}
+				ngModel.$setViewValue(html);
+			}
+		}
+	};
+});
+
+app.directive('initFocus', function() {
+    return {
+        restrict: 'A', // only activate on element attribute
+        link: function(scope, element, attrs) {
+			console.log('ok');
+			setTimeout(function() {
+				console.log('focus');
+				element.focus();
+			}, 0);
+        }
+    };
+});
