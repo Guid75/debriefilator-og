6210774a72009e29fe09a3c9cf8a971d3cc2391c
Francois Lecroart <peernohell@gmail.com>
2014-07-23 16:44:22 +0200

    fix wrong indent in api/sessions.js

--- api/sessions.js
+++ api/sessions.js
@@ -3,8 +3,8 @@ var express = require('express');
 var app = module.exports = express();
 
 app.use('/sessions', function (req, res) {
-  fs.readFile('data/sessions.json', function (err, data) {
-    if (err) return res.json(500, {error: 'no session file!'});
+	fs.readFile('data/sessions.json', function (err, data) {
+		if (err) return res.json(500, {error: 'no session file!'});
 
     res.json(JSON.parse(data).sessions);
   });
