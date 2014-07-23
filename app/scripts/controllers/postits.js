8edfda21952b9a7ccbaaa79463fc0feb044e8303
Francois Lecroart <peernohell@gmail.com>
2014-07-23 16:23:56 +0200

    fix jshint error (double quot)

--- app/scripts/controllers/postits.js
+++ app/scripts/controllers/postits.js
@@ -75,11 +75,11 @@ app.filter('luminance', function() {
 		}
 		lum = lum || 0;
 		// convert to decimal and change luminosity
-		var rgb = "#", c, i;
+		var rgb = '#', c, i;
 		for (i = 0; i < 3; i++) {
 			c = parseInt(hex.substr(i*2,2), 16);
 			c = Math.round(Math.min(Math.max(0, c + (c * lum)), 255)).toString(16);
-			rgb += ("00"+c).substr(c.length);
+			rgb += ('00'+c).substr(c.length);
 		}
 		return rgb;
 	};
